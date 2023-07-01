e611b4f2c462d40101fcc3e46c4a507dcf0bff43
Thomas Bakken <tbakken14@gmail.com>
2023-06-30 22:55:50 -0700

    Add supported country options

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import CurrencyExchangeService from './exchangeRate';
 import './css/styles.css';
 
-console.log(CurrencyExchangeService.getRates());
+function addCodes() {
+    CurrencyExchangeService.getCodes().then((data) => {
+        addCurrencyOptions(data.supported_codes);
+    });
+}
+
+function addCurrencyOptions(currencyCodes) {
+    currencyCodes = currencyCodes.sort((a, b) => (a[1] > b[1]) * 2 - 1);
+    let select = document.getElementById("currency");
+    for (const currencyCode of currencyCodes) {
+        let option = document.createElement("option");
+        select.appendChild(option);
+        option.setAttribute("value", currencyCode[0]);
+        option.setAttribute("name", currencyCode[1]);
+        option.append(currencyCode[1]);
+    }
+}
