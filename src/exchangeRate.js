cc986986045ee982a0a752c3a1122fa00cb962ce
Thomas Bakken <tbakken14@gmail.com>
2023-07-01 09:44:24 -0700

    Refactor api calls

--- src/exchangeRate.js
+++ src/exchangeRate.js
@@ -2,34 +2,29 @@
 export default class CurrencyExchangeService {
     static getCodes() {
         return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`)
-            .then((response) => {
-                if (!response.ok) {
-                    return response.json()
-                        .then(function (apiErrorMessage) {
-                            const errorMessage = `${response.status} ${apiErrorMessage["error-type"]}`;
-                            return Promise.reject(errorMessage);
-                        });
-                }
-                else {
-                    return response.json();
-                }
-            });
+            .then((response) => this.handleResponse(response));
 
     }
     static getConversion(targetCurrency, amount) {
         const baseCurrency = "USD";
         return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}/${amount}`)
-            .then((response) => {
-                if (!response.ok) {
-                    return response.json()
-                        .then(function (apiErrorMessage) {
-                            const errorMessage = `${response.status} ${apiErrorMessage["error-type"]}`;
-                            return Promise.reject(errorMessage);
-                        });
-                }
-                else {
-                    return response.json();
-                }
+            .then((response) => this.handleResponse(response));
+    }
+
+    static handleResponse(response) {
+        if (!response.ok) {
+            return this.rejectResponse(response);
+        }
+        else {
+            return response.json();
+        }
+    }
+
+    static rejectResponse(response) {
+        return response.json()
+            .then(function (apiErrorMessage) {
+                const errorMessage = `${response.status} ${apiErrorMessage["error-type"]}`;
+                return Promise.reject(errorMessage);
             });
     }
 }
