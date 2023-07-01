0f93732ad070c98fc5eaee7113060c6677c0aa68
Thomas Bakken <tbakken14@gmail.com>
2023-06-30 23:37:00 -0700

    Add error message display

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/styles.css';
 function addCodes() {
     CurrencyExchangeService.getCodes().then((data) => {
         addCurrencyOptions(data.supported_codes);
-    });
+    }).catch((errorMessage) => displayError(errorMessage));
 }
 
 function addCurrencyOptions(currencyCodes) {
@@ -19,13 +19,20 @@ function addCurrencyOptions(currencyCodes) {
     }
 }
 
+function displayError(errorMessage) {
+    let body = document.querySelector("body");
+    let p = document.createElement("p");
+    body.appendChild(p);
+    p.append(errorMessage);
+}
+
 function validateDollarInput(dollarInput) {
     dollarInput.value = /^\$?([1-9]\d*|0|)((?=\.)\.\d{0,2}|)$/.test(dollarInput.value) ?
         dollarInput.value : dollarInput.previousValue;
     dollarInput.previousValue = dollarInput.value;
 }
 
-function addEventListeners() {
+function addDollarInputEventListener() {
     let dollarInput = document.getElementById("dollarInput");
     dollarInput.previousValue = dollarInput.value;
     dollarInput.addEventListener("input", () => validateDollarInput(dollarInput));
